a9342555121e4342c20cb6592d2a51e77da8423d
JefersonFano <n00278667@upn.pe>
2025-05-01 17:22:27 -0500

    Actividades agregadas

--- app/src/main/java/com/example/sos_mujer/actividades/BienvenidaActivity.java
+++ app/src/main/java/com/example/sos_mujer/actividades/BienvenidaActivity.java
@@ -2,6 +2,7 @@ package com.example.sos_mujer.actividades;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,9 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.sos_mujer.R;
 import com.example.sos_mujer.clases.Menu;
 
-public class BienvenidaActivity extends AppCompatActivity implements Menu {
+public class BienvenidaActivity extends AppCompatActivity implements View.OnClickListener, Menu {
 
-    TextView lblSaludo;
+    TextView lblSaludo, lblContacto, lblReportar, lblPanico, lblPerfil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +27,19 @@ public class BienvenidaActivity extends AppCompatActivity implements Menu {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        lblContacto = findViewById(R.id.lblContacto);
+        lblReportar = findViewById(R.id.lblReportar);
+        lblPanico = findViewById(R.id.lblPanico);
+        lblPerfil = findViewById(R.id.lblPerfil);
 
         lblSaludo = findViewById(R.id.bieLblSaludo);
         String usuario = getIntent().getStringExtra("usuario");
         lblSaludo.setText("Hola " + usuario);
+
+        lblContacto.setOnClickListener(this);
+        lblReportar.setOnClickListener(this);
+        lblPanico.setOnClickListener(this);
+        lblPerfil.setOnClickListener(this);
     }
 
     @Override
@@ -38,5 +48,37 @@ public class BienvenidaActivity extends AppCompatActivity implements Menu {
         iMenu.putExtra("id",id);
         startActivity(iMenu);
         finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.lblContacto)
+            contacto();
+        else if (v.getId() == R.id.lblReportar)
+            reportar();
+        else if (v.getId() == R.id.lblPanico)
+            panico();
+        else if (v.getId() == R.id.lblPerfil)
+            perfil();
+    }
+
+    private void contacto() {
+        Intent iContacto = new Intent(this, ContactoActivity.class);
+        startActivity(iContacto);
+    }
+
+    private void reportar() {
+        Intent iReportar = new Intent(this, ReportarActivity.class);
+        startActivity(iReportar);
+    }
+
+    private void panico() {
+        Intent iPanico = new Intent(this, PanicoActivity.class);
+        startActivity(iPanico);
+    }
+
+    private void perfil() {
+        Intent iPerfil = new Intent(this, PerfilActivity.class);
+        startActivity(iPerfil);
     }
 }
