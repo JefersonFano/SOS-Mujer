c8fb1c11134c6fa43315dbe6d9bb94ccb351ba64
JefersonFano <n00278667@upn.pe>
2025-05-03 19:23:01 -0500

    Commit Jeferson

--- app/src/main/java/com/example/sos_mujer/actividades/PerfilActivity.java
+++ app/src/main/java/com/example/sos_mujer/actividades/PerfilActivity.java
@@ -1,6 +1,8 @@
 package com.example.sos_mujer.actividades;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.sos_mujer.R;
 
-public class PerfilActivity extends AppCompatActivity {
+public class PerfilActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnGuardar, btnCancelar, btnCerrarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,32 @@ public class PerfilActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnGuardar = findViewById(R.id.perfilBtnGuardar);
+        btnCancelar = findViewById(R.id.perfilBtnCancelar);
+        btnCerrarSesion = findViewById(R.id.perfilBtnCerrarSesion);
+
+        btnGuardar.setOnClickListener(this);
+        btnCancelar.setOnClickListener(this);
+        btnCerrarSesion.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.perfilBtnGuardar)
+            guardar();
+        else if (v.getId() == R.id.perfilBtnCancelar)
+            cancelar();
+        else if (v.getId() == R.id.perfilBtnCerrarSesion)
+            cerrarSesion();
+    }
+
+    private void guardar() {
+    }
+
+    private void cancelar() {
+        System.exit(1);
+    }
+
+    private void cerrarSesion() {
     }
 }
